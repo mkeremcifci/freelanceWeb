6fbb4ccedff378f8ec0faacc5e321596d267bde0
Mustafa Kerem Çifçi <cifcimustafakerem@gmail.com>
2025-06-30 18:25:06 +0300

    update deployment configurations and environment setup

--- backend/deneme.js
+++ backend/deneme.js
@@ -1,28 +1,40 @@
-import { text } from 'express';
-import nodemailer from 'nodemailer'
+import { MongoClient, ServerApiVersion } from "mongodb";
 
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: "freelanceallinfo@gmail.com",
-        pass:"tzzaykleigjbwytf",
-    },
+const uri = "mongodb+srv://cifcimustafakerem:BGRqzWP8l3ATZeru@cluster0.bedtx0q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
-const mailOptions = {
-    from: 'freelanceallinfo@gmail.com',
-    to: 'rumeyysaak@gmail.com',
-    subject: "Test mail",
-    text: "Test yazısı",
-};
+async function run() {
+  try {
+    await client.connect();
 
-transporter.sendMail(mailOptions, (error, info) => {
-    if(error){
-        console.error(error);
-    }
-    else{
-        console.log("Mail yollandı");   
-    }
-})
+    // Bağlandığımız db'yi seçiyoruz (örneğin "mydatabase")
+    const database = client.db("freelanceWeb");
+
+    // "users" koleksiyonunu seçiyoruz
+    const users = database.collection("users");
+
+    // Eklenecek kullanıcı verisi
+    const newUser = {
+      name: "Mustafa Kerem Çifçi",
+      email: "mustafa@example.com",
+      age: 25,
+      createdAt: new Date()
+    };
+
+    // Veri ekleme (insertOne)
+    const result = await users.insertOne(newUser);
+
+    console.log(`Yeni kullanıcı eklendi, _id: ${result.insertedId}`);
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
--- backend/index.js
+++ backend/index.js
@@ -2,22 +2,25 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import session from 'express-session';
+import dotenv from 'dotenv';
 
 import router from './routes/index.js';
 import sessionConfig from './configs/sessionConfig.js';
+
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://freelance-m09xnpipg-cifcimustafakerem-gmailcoms-projects.vercel.app",
     credentials: true
 }));
 
 app.use(express.json());
 app.use(session(sessionConfig));
 
-mongoose.connect("mongodb://localhost:27017/freelanceWeb")
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use('/',router);
 
